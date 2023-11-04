cee9bb400774776dd63f3d613fd0be4550ff4bf6
Oscar Neira Vaca <74775040+mrphaox@users.noreply.github.com>
2023-11-04 10:33:35 -0500

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center">
         <Image src="/icon.png" width={50} height={50} alt="logo" />
         <ul className="md:flex gap-8 items-center font-semibold text-[14px] hidden">
-          <li>Home</li>
+          <li>Inicio</li>
           <li className="bg-gray text-white px-6 py-2 rounded-3xl">About</li>
           <li>Comida</li>
           <li>Platos</li>
